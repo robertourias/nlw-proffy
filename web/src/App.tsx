82e7c82c2ebcfec4c0d8d59482ff4cd1bccc95c2
Roberto Nicoletti <roberto.urias@gmail.com>
2020-08-04 19:22:23 -0300

    estilo global e imagens

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import './assets/global.css';
+
 function App() {
   return (
     <div className="App">
-      <h1>hello word</h1>
+      <h1>Hello word</h1>
+
+      <button type="button">
+        Clica em mim
+      </button>
     </div>
   );
 }
