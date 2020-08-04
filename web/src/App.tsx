de771ba5e8a67f599b6e46a500d07607bad0be5c
Roberto Nicoletti <roberto.urias@gmail.com>
2020-08-04 20:17:29 -0300

    layout responsivo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 
 import './assets/global.css';
 
+import Landing from './pages/Landing';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello word</h1>
-
-      <button type="button">
-        Clica em mim
-      </button>
-    </div>
+    <Landing />
   );
 }
 
