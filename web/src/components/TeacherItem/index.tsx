627f358f3b020251747f9584957944612922eddb
Roberto Nicoletti <roberto.urias@gmail.com>
2020-08-08 18:53:09 -0300

    finalizando com contador de conexões

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './styles.css';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api';
 
 export interface Teacher {
     id: number;
@@ -18,6 +19,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
 
     return (
         <article className="teacher-item">
@@ -36,7 +43,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
                             Preço/Hora
                             <strong>R$ {teacher.cost}</strong>
                         </p>
-                        <a target="_blank" href={`https://wa.me/${teacher.whatsapp}`}>
+                        <a onClick={createNewConnection} target="_blank" href={`https://wa.me/${teacher.whatsapp}`}>
                             <img src={whatsappIcon} alt="Whatsapp"/>
                             Entrar em contato
                         </a>
